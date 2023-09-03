68ce3308133ba931ff687c4e63ab6dbe6f3cdccd
iamalexneumann <iamalexneumann@yandex.ru>
2023-09-03 14:07:27 +0300

    Adds a solution to task 3.7

--- data.php
+++ data.php
@@ -14,35 +14,41 @@ $ads = [
         'category' => $categories['boards'],
         'price' => '10999',
         'img_url' => 'img/lot-1.jpg',
+        'dt' => '2023-09-03 14:00'
     ],
     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => $categories['boards'],
         'price' => '159999',
         'img_url' => 'img/lot-2.jpg',
+        'dt' => '2023-10-03'
     ],
     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => $categories['attachment'],
         'price' => '8000',
         'img_url' => 'img/lot-3.jpg',
+        'dt' => '2023-09-05'
     ],
     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => $categories['boots'],
         'price' => '10999',
         'img_url' => 'img/lot-4.jpg',
+        'dt' => '2023-09-25'
     ],
     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => $categories['clothing'],
         'price' => '7500',
         'img_url' => 'img/lot-5.jpg',
+        'dt' => '2023-09-04'
     ],
     [
         'title' => 'Маска Oakley Canopy',
         'category' => $categories['other'],
         'price' => '5400',
         'img_url' => 'img/lot-6.jpg',
+        'dt' => '2023-12-02'
     ],
 ];
--- functions.php
+++ functions.php
@@ -3,3 +3,17 @@ function format_price($price): string
 {
     return number_format(ceil($price), 0, '', ' ') . ' ₽';
 }
+
+function get_dt_range($datetime):array
+{
+    date_default_timezone_set('Europe/Moscow');
+
+    $interval = strtotime($datetime) - time();
+    $hours = floor($interval / 3600);
+    $minutes = floor(($interval - ($hours * 3600)) / 60);
+
+    return [
+        str_pad($hours, 2, '0', STR_PAD_LEFT),
+        str_pad($minutes, 2, '0', STR_PAD_LEFT)
+    ];
+}
